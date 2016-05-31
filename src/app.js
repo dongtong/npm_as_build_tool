5127818d20f880d66d9ba0be01cf485757d461b7
dongtong <jinhu.tung@gmail.com>
2016-05-31 23:45:26 +0800

    add browserify script

---
+++ src/app.js
@@ -0,0 +1,4 @@
+var Task = require('./js/task.js');
+
+var task = new Task("foobar", 30);
+console.log(task.toString());
---
+++ src/js/task.js
@@ -0,0 +1,10 @@
+function Task(title, weight) {
+	this.title = title;
+	this.weight = weight;
+}
+
+Task.prototype.toString = function () {
+	return "Title: " + this.title + " - weight: " + this.weight;
+}
+
+module.exports = Task;
